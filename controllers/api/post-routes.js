5f7eae78dc9a72222872cabf6ed0ec459e2f880c
nickhill2380 <nickhill2380@gmail.com>
2021-02-02 20:34:05 -0600

    added a post update route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,107 @@
+const router = require('expres').Router();
+const {Post, User, Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes:[
+            'title',
+            'text_area'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes:['comment_text'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'title',
+            'text_area'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['comment_text'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if (!postData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(postData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+router.post('/', (req,res) => {
+    Post.create({
+        title: req.body.title,
+        text_area: req.body.text_area,
+        user_id: req.session.user_id
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req,res) => {
+    Post.update(
+        {
+           title: req.body.title,
+           text_area: req.body.text_area 
+        },
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    )
+    .then(postData => {
+        if(!postData) {
+            res.status(404).json({message: 'No post found with that id'});
+            return; 
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
